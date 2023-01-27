922053261c6b33777bfe63af2021f0bfa63594c5
Amine <111646702+hidriamin@users.noreply.github.com>
2023-01-27 20:15:50 +0100

    Animations and more.
    Additions include animations,smooth scroll and making the site better looking.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//navbar2 show and hide
 var mainNavbar = document.getElementById("navbar");
 var secondaryNavbar = document.getElementById("navbar2");
 
@@ -20,6 +21,7 @@ window.onscroll = function() {
     }
     prevScrollPosition = curScrollPosition;
 }
+//cars image at the bottom
 const background = document.getElementById("background");
 function mouseOn(x){
 background.style.backgroundImage = "url(https://www.mercedes-benz.com/en/vehicles/mercedes-benz-classic/classiccenter/_jcr_content/root/imagejumper_10503641/parsys/imagejumperitem/image/MQ7-0-image-20211118123015/09-mercedes-benz-classic-centerfellbach-3400x1912.jpeg;)";
@@ -34,7 +36,19 @@ logo.onclick = function hoverLogo(x){
 logo.addEventListener('animationend', () => {
     logo.className = "before-hover";
 });
-
+//animation:
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else{
+            entry.target.classList.remove("show");
+        }
+    })
+} )
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 //const buttons = document.querySelectorAll('[data-popup-target]');
 //buttons.forEach(button => {S
    // button.addEventListener('click', event => {
